87f55e18d9c87565b1c20347af3031ac20480b42
andersonsbenicio <andersonsbenicio@gmail.com>
2024-06-08 14:22:48 -0300

    Refactory Project

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,42 +1,35 @@
-//before - antes de todos os testes
-//beforeEach - antes de cada teste
-//after - depois de todos os testes
-//afterEach - depois de cada teste
+/// <reference types="cypress" />
+const element = require("../fixtures/login.json");
 
+//beforeEach - antes de cada teste
 beforeEach(() => {
   cy.visit("https://automacao.qacoders-academy.com.br/login");
 });
 
+afterEach(() => {
+  cy.screenshot();
+});
+
 describe("Login", () => {
   it("Login com sucesso", () => {
-    cy.get("#email").type("sysadmin@qacoders.com");
-    cy.get("#password").type("1234@Test");
-    cy.get("#login").click();
-
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq("/login");
-    });
-
-    cy.get(".css-1nvbq2d").should("be.visible");
+    const email = Cypress.env("EMAIL");
+    const password = Cypress.env("PASSWORD");
+    cy.Login(email, password);
   });
 
   it("Login com e-mail válido e a senha inválida", () => {
-    cy.get("#email").type("sysadmin@qacoders.com");
-    cy.get("#password").type("1234@");
-    cy.get("#login").click();
-
-    cy.get(".MuiPaper-root").should("be.visible");
-    cy.get(".MuiPaper-root").should("have.text", "E");
-    //E-mail ou senha informados são inválidos.
+    cy.get(element.input_email).type(Cypress.env("EMAIL"));
+    cy.get(element.input_password).type(Cypress.env("PASSWORD_INVALIDO"));
+    cy.get(element.btn_login).click();
+    cy.MsgLoginFalho();
   });
 
   it("Login com e-mail válido e a senha inválida", () => {
-    cy.get("#email").type("sysadm@qacoders.com");
-    cy.get("#password").type("1234@Test");
-    cy.get("#login").click();
-
-    cy.get(".MuiPaper-root").should("be.visible");
-    cy.get(".MuiPaper-root").should("have.text", "E");
-    //E-mail ou senha informados são inválidos.
+    cy.get(element.input_email).type(Cypress.env("EMAIL_INVALIDO"));
+    cy.get(element.input_password).type(Cypress.env("PASSWORD"));
+    cy.get(element.btn_login).click();
+    cy.MsgLoginFalho();
   });
 });
+
+//E-mail ou senha informados são inválidos.
